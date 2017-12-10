11f6c2a0399813c5a1f587687e5199249c34da0a
Jeon Jeongho <maczniak@gmail.com>
2017-12-10 02:09:50 +0000

    fix the mining fee calculation

--- src/miner.ts
+++ src/miner.ts
@@ -3,20 +3,23 @@ import { bufferToHex } from "./buffer";
 import { blockMerkleRoot } from "./merkle";
 import { CoinbaseTx, Tx, isNormalTx } from "./tx";
 import { Script } from "./types";
-import { CoinsView } from "./coins";
+import { CoinsView, addCoins } from "./coins";
 
 const BLOCK_REWARD = 50;
 
-export function createNewBlock(txOutScript: Script, txs: Tx[] = [], prevHash = Buffer.alloc(0), view: CoinsView): Block {
+export function createNewBlock(txOutScript: Script, txs: Tx[] = [], prevHash = Buffer.alloc(0), coinsview: CoinsView): Block {
     // FIXME: Add the mining fee to the coinbase transaction.
+    let view = new CoinsView(coinsview);
     let valueInTotal = 0;
     let valueOutTotal = 0;
+    // assume that transactions precede the others that depends on their outputs within a block.
     for (const tx of txs) {
         if (isNormalTx(tx)) {
             for (const txinput of tx.inputs) {
                 valueInTotal += view.getCoin(txinput.prevOut).out.value;
             }
             valueOutTotal += tx.valueOut;
+            addCoins(view, tx);
         }
     }
     const reward = new CoinbaseTx([{
